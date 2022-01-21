dac31b564db5730e7370546e269c35a73c9b9c2f
AndrewWladis <andrewwladis@gmail.com>
2022-01-21 09:28:00 -0500

    first commit

---
+++ unused.js
@@ -0,0 +1,20 @@
+//const imdb = require('imdb-api');
+
+/*fetch(`https://imdb-api.com/en/API/Title/${apiKey}/${movieid}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            apiJSONResponse = result
+            console.log(apiJSONResponse)
+        })
+        .catch(error => console.log('error', error));
+
+        fetch(`https://imdb-api.com/en/API/Title/${apiKey}/${movieid}`).then(
+            function(u){ return u.json();}
+          ).then(
+            function(json){
+                apiJSONResponse = json;
+            }
+          )
+    let obj = await (await fetch(`https://imdb-api.com/en/API/Title/${apiKey}/${movieid}`)).json();
+    console.log(obj);
+    return obj;*/
